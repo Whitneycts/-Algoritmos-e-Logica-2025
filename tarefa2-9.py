312ec69b490b99e0b9398680120770e4fc0314c6
Whitney Costa <whitneycosta@proton.me>
2025-10-05 23:45:28 -0300

    adicionando tarefa 2 aula 9

---
+++ tarefa2-9.py
@@ -0,0 +1,21 @@
+# Lista de frutas
+frutas = ["maçã", "banana", "uva", "laranja"]
+
+# O laço for vai repetir uma vez para cada fruta da lista
+for fruta in frutas:         # "fruta" é a variável que recebe cada item da lista
+    print("Eu gosto de", fruta) # usamos a variável dentro do print
+
+
+# Explicação:
+# 1. O 'for' em Python serve para repetir algo várias vezes,
+#    passando por cada item de uma lista, string, dicionário, etc.
+#
+# 2. A palavra 'in' é OBRIGATÓRIA -> ela mostra de onde vêm os dados.
+#
+# 3. Em cada repetição, o Python pega um item da sequência e guarda
+#    na variável que você escolheu (ex.: fruta, letra, chave).
+#
+# 4. O bloco de código dentro do for (indentado com espaços) é executado
+#    uma vez para cada item.
+#
+# 5. Quando acabam os itens, o laço for termina sozinho.
